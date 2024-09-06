66827c8a85be8784b74ec6d5d4108327906623d0
AmauriBlanco <blancoamauri@gmail.com>
2024-09-06 20:00:53 -0300

    up

---
+++ src/app/detalhes/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Detalhes filmes ou séries</h1>
+        </div>
+    );
+}
---
+++ src/app/filmes/page.tsx
@@ -0,0 +1,7 @@
+export default function Filmes() {
+    return (
+        <div>
+            <h1>Filmes</h1>
+        </div>
+    );
+}
