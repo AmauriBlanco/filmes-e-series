35f8da5175a6f28f922ae1e4782d6d6a20248267
AmauriBlanco <blancoamauri@gmail.com>
2024-09-08 23:11:09 -0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
-export const images = {
-    remotePatterns: [
+module.exports = {
+    images: {
+      remotePatterns: [
         {
-            protocol: "https",
-            hostname: "image.tmdb.org",
+          protocol: "https",
+          hostname: "image.tmdb.org",
         },
-    ],
-};
+      ],
+    },
+    basePath: '/filmes-e-series',  
+    assetPrefix: '/filmes-e-series/', 
+    trailingSlash: true,
+  };
+  
